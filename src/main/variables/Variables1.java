7fcd7baaa1f9f131e63e6aedea71176e2b9b28b5
rodrigocg123 <rocasgut@gmail.com>
2025-09-24 10:23:17 +0200

    declaracion de variables

---
+++ src/main/variables/Variables1.java
@@ -0,0 +1,27 @@
+package main.variables;
+
+public class Variables1 {
+    public static void main(String[] args) {
+        byte mybyte;
+        short myshot;
+        int myint;
+
+        myint = 18;
+        mybyte = -123;
+        myshot = 190;
+        
+        long isbn= 978849838708L;
+        float decimal= 1.1f;
+        double decimalLargo = 1.111;
+
+        char caracter = 'a';
+        char otroCaracter = '*';
+
+        String saludo = "Hola que tal";
+        String despedida = "Adios";
+
+        boolean decision1 = true;
+        boolean decision2 = false;
+
+    }
+}
